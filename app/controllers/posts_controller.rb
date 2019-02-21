43130f223a0f5a09d755ac80a0a36904970978db
Steve Albonico <salbonico@gmail.com>
2019-02-21 02:19:28 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,3 +27,14 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 end
+
+private
+ 
+ 
+# We pass the permitted fields in as *args;
+# this keeps `post_params` pretty dry while
+# still allowing slightly different behavior
+# depending on the controller action
+def post_params(*args)
+  params.require(:post).permit(*args)
+end
